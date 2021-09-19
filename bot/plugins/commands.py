4557f97309882713e9020581f9ad0d746b5b1f95
kalana kt <86665964+kalanakt@users.noreply.github.com>
2021-09-19 15:03:59 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption =  " 🔰Spacious Universe🔰 \n     @TMWAD",
+                caption =  "Daily Series Updates : @Series2Day \n Main Group : @Spaciousuniversegroup1",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
